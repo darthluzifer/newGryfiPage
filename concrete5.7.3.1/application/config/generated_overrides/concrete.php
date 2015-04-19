1fb8b6f7d3c5937bdb78b400edd135dee21b3bf0
Lucius <lucius.bachmann@gmx.ch>
2015-04-19 16:30:23 +0200

    now .gitignore points on the files themselves

---
+++ concrete5.7.3.1/application/config/generated_overrides/concrete.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * -----------------------------------------------------------------------------
+ * Generated 2015-04-06T20:51:38+02:00
+ *
+ * @item      misc.seen_introduction
+ * @group     concrete
+ * @namespace null
+ * -----------------------------------------------------------------------------
+ */
+return array(
+    'locale' => 'de_DE',
+    'site' => 'newgryfipage',
+    'version_installed' => '5.7.3.1',
+    'misc' => array(
+        'access_entity_updated' => 1428345997,
+        'latest_version' => '5.7.3.1',
+        'seen_introduction' => true
+    )
+);
