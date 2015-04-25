e3cbc2c27cb8a166ae0994437d998cbd28060c44
Lucius <lucius.bachmann@gmx.ch>
2015-04-25 12:35:17 +0200

    javascript for modal

--- concrete5.7.3.1/application/blocks/basic_table_block/view.php
+++ concrete5.7.3.1/application/blocks/basic_table_block/view.php
@@ -45,3 +45,44 @@ $show_graph = (count($optionNamesAbbrev) && !$_GET['dontGraphPoll'] && $totalVot
     
 
 </div>
+
+
+
+<script type="text/javascript">
+$(function() {
+
+	var $db = $('div[data-discussion-block-id=<?php echo $b->getBlockID()?>]'),
+		$dialog = $db.find('div[data-form=discussion]'),
+		$addTopic = $db.find('button[data-action=add-conversation]'),
+		$orderBy = $db.find('select[data-select=order]');
+
+	$orderBy.on('change', function() {
+
+		window.location.href = $(this).find('option:selected').attr('data-sort-url');
+	});
+	$('form[data-form=composer]').ccmcomposer({
+		onExit: function() {
+			$dialog.dialog('close');
+		},
+		autoSaveEnabled: false,
+		publishReturnMethod: 'ajax',
+		onPublish: function(r) {
+			window.location.href = r.redirectURL;
+		}
+	});
+
+	$addTopic.on('click', function() {
+		$dialog.dialog({
+			modal: true,
+			width: 400,
+			height: 540,
+			title: '<?php echo t("Add Topic")?>',
+			open: function() {
+				var $buttons = $dialog.find('.dialog-buttons').hide().clone(true,true);
+				$(this).dialog('option', 'buttons', [{}]);
+				$(this).closest('.ui-dialog').find('.ui-dialog-buttonset').html('').append($buttons.show());
+			}
+		});
+	});
+});
+</script>
