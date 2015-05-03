440ac7b2f0578394994da8d2f439a8528e5ee0ae
Lucius <lucius.bachmann@gmx.ch>
2015-05-03 15:08:54 +0200

    add sql

--- concrete5.7.3.1/packages/email_list_signup/mail/signup_confirmation_admin_notify.php
+++
@@ -1,7 +0,0 @@
-<?php  defined('C5_EXECUTE') or die(_("Access Denied."));
-
-$body = t("New email signup has been confirmed:
-
-%s
-
-", $signup_email);
--- concrete5.7.3.1/packages/email_list_signup/mail/signup_submission_admin_notify.php
+++
@@ -1,7 +0,0 @@
-<?php  defined('C5_EXECUTE') or die(_("Access Denied."));
-
-$body = t("New email signup (has not been confirmed yet):
-
-%s
-
-", $signup_email);
