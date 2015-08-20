e6411c9a6e42a4242633cdb49f2456f1ded0a154
lucius <lucius.bachmann@gmx.ch>
2015-08-20 22:31:43 +0200

    table block working

--- concrete5.7.3.1/application/blocks/basic_table_block/views/table_view.php
+++ concrete5.7.3.1/application/blocks/basic_table_block/views/table_view.php
@@ -2,12 +2,11 @@
 use Application\Block\BasicTableBlock\FieldTypes\SelfSaveInterface;
 ?>
 
-<div class="poll">
+<div class="table-responsive">
+ 
  
 
-<div id="surveyQuestion" class="form-group">
-           
-        </div>
+
 
         
         
@@ -20,7 +19,7 @@ use Application\Block\BasicTableBlock\FieldTypes\SelfSaveInterface;
 			
 			<?php  echo "<button type='submit' value='' class='btn inlinebtn actionbutton add'><i class ='fa fa-plus'> </i></button>"?>
 		</form>
-        <table id='<?php echo $controller->getHTMLId(); ?>' class="table">
+        <table id='<?php echo $controller->getHTMLId(); ?>' class="table table-striped table-bordered table-hover">
         <thead>
 	        <tr>
 	        <?php 
@@ -31,7 +30,7 @@ use Application\Block\BasicTableBlock\FieldTypes\SelfSaveInterface;
 	        	?>
 	        	<th width='20%'  data-column-id="commands" data-formatter="commands" data-sortable="false"><?php echo t('Aktion'); ?></th>
 	        	<?php }else{?>
-	        	<th data-column-id='<?php $type->getPostName()?>'><?php echo $fieldname ?></th>
+	        	<th data-column-id='<?php echo $type->getPostName();?>'><?php echo $fieldname; ?></th>
 	        <?php 
 	        	}
 	        }?>
@@ -78,22 +77,33 @@ use Application\Block\BasicTableBlock\FieldTypes\SelfSaveInterface;
 </div>
 
 <script type='text/javascript'>
-actioncellhtml['<?php echo $controller->getHTMLId();?>'] = [];
-actioncellhtmltemp['<?php echo $controller->getHTMLId();?>'] = [];
+if(window.actioncellhtml == undefined){
+	window.actioncellhtml = {};
+}
+if(window.actioncellhtmltemp == undefined){
+	window.actioncellhtmltemp = {};
+}
+window.actioncellhtml['<?php echo $controller->getHTMLId();?>'] = [];
+window.actioncellhtmltemp['<?php echo $controller->getHTMLId();?>'] = [];
 $(document).ready(function(e){ 
 	
 	$('#<?php echo $controller->getHTMLId();?> .actioncell').each(function(e){
-			actioncellhtml.push($(this).html());
+			window.actioncellhtml['<?php echo $controller->getHTMLId();?>'].push($(this).html());
+			
 		});
 	
 	
 	$('#<?php echo $controller->getHTMLId();?>').bootgrid({
+				columnSelection : false,
+				rowCount:-1,
 				formatters : {
 						commands: function(column, row){
 								var html = window.actioncellhtml['<?php echo $controller->getHTMLId();?>'].pop();
 								window.actioncellhtmltemp['<?php echo $controller->getHTMLId();?>'].push(html);
-								if(window.actioncellhtml['<?php echo $controller->getHTMLId();?>'].length = 0){
-										window.actioncellhtml = actioncellhtmltemp;
+								if(window.actioncellhtml['<?php echo $controller->getHTMLId();?>'].length == 0){
+										$(window.actioncellhtmltemp['<?php echo $controller->getHTMLId();?>']).each(function(e){
+												window.actioncellhtml['<?php echo $controller->getHTMLId();?>'].push(window.actioncellhtmltemp['<?php echo $controller->getHTMLId();?>'].pop());
+											});
 										window.actioncellhtmltemp['<?php echo $controller->getHTMLId();?>'] = [];
 									}
 								return html;
