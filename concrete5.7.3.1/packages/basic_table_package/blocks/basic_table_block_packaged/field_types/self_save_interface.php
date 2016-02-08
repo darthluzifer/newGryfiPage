53727317c228e36a305b4a12247fdf4e23caa9c9
Lucius <lucius.bachmann@gmx.ch>
2016-02-08 22:25:42 +0100

    first try implementation of dropdownlinkfield und dropdownmultilinkfield,, now changed the right file, delete fieldtypes folder in block

--- concrete5.7.3.1/packages/basic_table_package/blocks/basic_table_block_packaged/field_types/self_save_interface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Concrete\Package\BasicTablePackage\Block\BasicTableBlockPackaged\FieldTypes;
-
-interface SelfSaveInterface{
-	//some function that saves the values itself, not by the block
-	public function saveValues($value);
-}
