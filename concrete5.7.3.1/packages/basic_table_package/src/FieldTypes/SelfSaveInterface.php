23856d3f64da42cbfabbbf9d5fc2c83bbfb6643f
BacLuc <lucius.bachmann@gmx.ch>
2016-04-07 16:40:10 +0200

    remove basic table block from newGryfiPage Project

--- concrete5.7.3.1/packages/basic_table_package/src/FieldTypes/SelfSaveInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Concrete\Package\BasicTablePackage\Src\FieldTypes;
-
-interface SelfSaveInterface{
-	//some function that saves the values itself, not by the block
-	public function saveValues($value);
-}
