feceb0d91388502b8f84cf48ab9c1efbdda63f07
BacLuc <lucius.bachmann@gmx.ch>
2016-08-05 16:07:32 +0200

    refactor design package to now load from seperate github directory.

--- concrete5/packages/theme_stucco/blocks/feature/templates/stucco_hover_description/view.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('.ccm-block-feature-item-hover-wrapper').tooltip();
-    $('.ccm-block-feature-item-hover-wrapper').on('shown.bs.tooltip', function () {
-        $('.tooltip').addClass('animated bounceInDown');
-    }).on('hide.bs.tooltip', function () {
-        $('.tooltip').removeClass('animated bounceInDown');
-    });
-});
