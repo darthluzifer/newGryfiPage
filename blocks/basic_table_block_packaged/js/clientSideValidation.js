7c2e1b1d653a0a90c2148c833e528f695aa20348
BacLuc <lucius.bachmann@gmx.ch>
2016-09-25 13:10:49 +0100

    add logic on type validation

--- blocks/basic_table_block_packaged/js/clientSideValidation.js
+++ blocks/basic_table_block_packaged/js/clientSideValidation.js
@@ -9,6 +9,7 @@
             });
             $(this).addClass('bacluc-parsley-activated');
 
+            //on cancel click, turn of validation
             $(this).find('input[name="cancel"]').click(function(e){
                 //turn off html validation
                 $(this).parent('form').attr('novalidate','true')
@@ -17,7 +18,12 @@
 
                 //continue bubbling
                 return true;
-            })
+            });
+
+            //validate on input
+            $(this).find('input,textarea,select').on('change, keyup', function(e){
+                $(this).parsley().validate();
+            });
         }
     });
 })(jQuery);
