e8b4209451175c3c294e80d4d07c556bf83531d1
BacLuc <lucius.bachmann@gmx.ch>
2017-03-17 16:17:46 +0100

    delete owncloud update files

--- owncloud/data/updater_backup/8.0.2.0-c2324daf/apps/user_external/appinfo/app.php
+++
@@ -1,6 +0,0 @@
-<?php
-OC::$CLASSPATH['OC_User_IMAP']='user_external/lib/imap.php';
-OC::$CLASSPATH['OC_User_SMB']='user_external/lib/smb.php';
-OC::$CLASSPATH['OC_User_FTP']='user_external/lib/ftp.php';
-OC::$CLASSPATH['OC_User_Concrete5']='user_external/lib/concrete5.php';
-OC_User::setupBackends();
