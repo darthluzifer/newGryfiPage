640074cc8030c9c4905416645032c8547e908a61
BacLuc <lucius.bachmann@gmx.ch>
2017-08-13 19:37:37 +0200

    add shell script to pull from all subtree repositories master branch

--- subtree_pull_develop.sh
+++ subtree_pull_develop.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 branch="develop";
 git checkout -b create_release
-git subtree pull --prefix=concrete5/packages/basic_table_package/ git@github.com:BacLuc/basic_table_package.git $branch
-git subtree pull --prefix=concrete5/packages/bacluc_gryfenberg_theme/ git@github.com:BacLuc/bacluc_gryfenberg_theme.git $branch
-git subtree pull --prefix=concrete5/packages/bacluc_person_package/ git@github.com:BacLuc/bacluc_person_package.git $branch
-git subtree pull --prefix=concrete5/packages/bacluc_event_package/ git@github.com:BacLuc/bacluc_event_package.git $branch
-git checkout $branch
-git merge create_release
-git commit -m "pulled latest commit from $branch of subtrees and merged into $branch"
+
+rm -r concrete5/packages/basic_table_package/
+git add *
+git commit -m "removed directory"
+git subtree add --prefix=concrete5/packages/basic_table_package/ git@github.com:BacLuc/basic_table_package.git $branch
+
+rm -r concrete5/packages/bacluc_gryfenberg_theme/
+git add *
+git commit -m "removed directory"
+git subtree add --prefix=concrete5/packages/bacluc_gryfenberg_theme/ git@github.com:BacLuc/bacluc_gryfenberg_theme.git $branch
+
+rm -r concrete5/packages/bacluc_person_package/
+git add *
+git commit -m "removed directory"
+git subtree add --prefix=concrete5/packages/bacluc_person_package/ git@github.com:BacLuc/bacluc_person_package.git $branch
+
+rm -r concrete5/packages/bacluc_event_package/
+git add *
+git commit -m "removed directory"
+git subtree add --prefix=concrete5/packages/bacluc_event_package/ git@github.com:BacLuc/bacluc_event_package.git $branch
+
+git checkout try_pull_master
+git merge create_release -m "pulled latest commit from $branch of subtrees and merged into $branch"
 git branch -d create_release
