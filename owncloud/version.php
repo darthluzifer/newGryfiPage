47c587abffc0d34188cbc83e75f4041280ddd4a2
BacLuc <lucius.bachmann@gmx.ch>
2017-08-22 21:20:54 +0200

    removed owncloud, added nextcloud

--- owncloud/version.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$OC_Version = array(8,2,3,2);
-$OC_VersionString = '8.2.3';
-$OC_Edition = '';
-$OC_Channel = 'stable';
-$OC_VersionCanBeUpgradedFrom = array(8,1);
-$OC_Build = '2016-03-08T14:15:07+00:00 fc18c0506afc85ccfda96c0a17e0616634952701';
